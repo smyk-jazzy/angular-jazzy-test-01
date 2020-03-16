5a2e72563d27527f6aafdb85bfd401484dc842cc
smyk-jazzy <tomasz.jeziorny@wp.pl>
2020-03-16 21:57:04 +0100

    Change detection - 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular change detection';
+  name = 'change detection' ;
+  counter = 0;
+
+  constructor(){
+    // setInterval(() => this.incrementCounter(), 2000)
+  }
+
+  getInfo(){
+    const msg = 'Metoda getInfo(); counter: ' + this.counter;
+    console.log(msg);
+    return msg;
+  }
+
+  getCounter(){
+    console.log('getCounter(): ' + this.counter)
+    return this.counter
+  }
+
+  incrementCounter(){
+    this.counter++;
+    console.log("incrementCounter(): " + this.counter)
+  }
 }
